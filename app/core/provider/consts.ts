17ed5bf3816831252699e90f12d727278441bc9b
steelbull <steelbulloff@gmail.com>
2024-07-20 18:21:33 +0500

    Fix

--- app/core/provider/consts.ts
+++ app/core/provider/consts.ts
@@ -1,10 +1,19 @@
+import { AnyTxtRecord } from "dns";
+
 export const CELCY: string = "°";
 export let bgcode = "";
 export const Errors = {
   errorCity: "Не правильное название города",
 };
 
-export const IsNewCity =  window.localStorage.getItem("NewCity")
-export const RemNewCity = window.localStorage.removeItem("NewCity")
+export let IsNewCity:any
 
-export let ActiveError: any = [];
+export let RemNewCity :void 
+
+if (typeof window !== 'undefined') {
+  // Safely access localStorage
+ IsNewCity = localStorage.getItem("NewCity");
+ RemNewCity = localStorage.removeItem("NewCity");
+}
+
+export let ActiveError = [];
